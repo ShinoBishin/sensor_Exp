46f214e62ab79da26741048d28f06fc12033d557
kohey0701 <kohey0701@gmail.com>
2021-01-07 21:51:26 +0900

    LED点灯連携と無限ループ停止処理実装

--- human_sensor.py
+++ human_sensor.py
@@ -12,8 +12,20 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(motion_pin,GPIO.IN)
 GPIO.setup(led_pin,GPIO.OUT)
 
-while True:
-    print(GPIO.input(motion_pin))
-    time.sleep(1)
+def main():
+    while True:
+        print(GPIO.input(motion_pin))
+        time.sleep(0.1)
+        if(GPIO.input(motion_pin) == 1):
+            GPIO.output(led_pin, GPIO.HIGH)
+        else:
+            GPIO.output(led_pin, GPIO.LOW)
+
+try:
+    main()
+except KeyboardInterrupt:
+    print("停止処理を受け付けました")
+
+GPIO.cleanup()
 
 # 人感センサーがこちらの動きを検知すると１を返す。
