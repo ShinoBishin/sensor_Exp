7ab50d1c28477c81ca4ccbfeb302186ad81ed4a0
kohey0701 <kohey0701@gmail.com>
2021-01-10 21:17:00 +0900

    人間往来回数検知マシンver.1.0

--- 7seg_humanSensor/hs_7seg_counter.py
+++ 7seg_humanSensor/hs_7seg_counter.py
@@ -8,35 +8,41 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(seg_pin, GPIO.OUT)
 GPIO.setup(hs_pin, GPIO.IN)
 
-seg_number = 9
+seg_number = 8
 i = 0
 
-def write_7seg():
-    global i
+# 7セグ表示プログラム(ドライバ用)
+def write_7seg(seg_pin,seg_number):
+    i = 0
     while i < 4:
-        GPIO.output(seg_pin[i], count & (0x01 << i))
+        GPIO.output(seg_pin[i], seg_number & (0x01 << i))
         i = i + 1
 
-# time.sleep(3)
-# GPIO.output(seg_pin, GPIO.LOW)
+# カウントアップ挙動の実験
+# while i <= 9:
+#     write_7seg(seg_pin, i)
+#     time.sleep(1)
+#     i = i + 1
 
+# メイン処理(人感センサー制御)
 try:
-    print("処理キャンセル：Ctrl+C")
-    seg_number = 1
+    num = 1
     count = 1
     while True:
         if(GPIO.input(hs_pin) == GPIO.HIGH):
-            print(str(count) + "回目の検知")
+            print(str(count) + "人発見しました")
             count = count + 1
-            write_7seg()
+            write_7seg(seg_pin, num)
             time.sleep(20)
+            num = num + 1
         else:
             print(GPIO.input(hs_pin))
-            time.sleep(3)
+            time.sleep(1)
 except KeyboardInterrupt:
-    print("終了処理中")
+    pass
 finally:
+    GPIO.output(seg_pin, GPIO.LOW)
     GPIO.cleanup()
     print("GPIOクリナップクリンミセス")
 
-GPIO.cleanup
+# GPIO.cleanup
