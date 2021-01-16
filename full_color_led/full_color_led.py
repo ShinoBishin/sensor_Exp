f4457362de55fc0ea5d15d5ed694f600d8a658ee
kohey0701 <kohey0701@gmail.com>
2021-01-16 19:23:11 +0900

    タクトスイッチによる色ごとの調色プログラム

--- full_color_led/full_color_led.py
+++ full_color_led/full_color_led.py
@@ -1,41 +1,66 @@
 import wiringpi as pi
 import time
 from mcp3002 import mcp3002
-from hsv_rgb import hsv_rgb
 
 green_pin = 18
 blue_pin = 23
 red_pin = 24
+btn_pin = 17
 
-saturation = 1.0
-value = 1.0
-vol_hue = 1.0
+red = 0
+blue = 0
+green = 0
+select = 0
 
 SPI_CH = 0
 READ_CH = 0
-
-hsv = hsv_rgb(vol_hue, saturation, value)
 
 pi.wiringPiSetupGpio()
 pi.pinMode(green_pin, 1)
 pi.pinMode(blue_pin, 1)
 pi.pinMode(red_pin, 1)
 
+pi.pinMode(btn_pin, 0)
+pi.pullUpDnControl(btn_pin, 2)
+
 pi.softPwmCreate(green_pin, 0, 100)
 pi.softPwmCreate(blue_pin, 0, 100)
 pi.softPwmCreate(red_pin, 0, 100)
 
 mcp3002 = mcp3002(pi, SPI_CH)
-while True:
-	val = mcp3002.read(pi, READ_CH)
-	tmp_hue = val / 1023
-	vol_hue = vol_hue * 0.9 + tmp_hue * 0.1
+try:
+	while True:
+		if(pi.digitalRead(btn_pin) == 0):
+			time.sleep(0.01)
+			if(select == 0):
+				select = 1
+				print("みどり")
+			elif(select == 1):
+				select = 2
+				print("あお")
+			elif(select == 2):
+				select = 0
+				print("あか")
+			while(pi.digitalRead(btn_pin) == 0):
+				time.sleep(0.01)
 
-	hsv.set_hsv(vol_hue, saturation, value)
-	(red, green, blue) = hsv.get_rgb()
+		val = mcp3002.read(pi, READ_CH)
+		tmp_val = val / 1023
 
-	pi.softPwmWrite(green_pin, int(green * 100))
-	pi.softPwmWrite(blue_pin, int(blue * 100))
-	pi.softPwmWrite(red_pin, int(red * 100))
+		if(select == 0):
+			red = tmp_val
+		elif(select == 1):
+			green = tmp_val
+		elif(select == 2):
+			blue = tmp_val
 
-	time.sleep(0.01)
+
+		pi.softPwmWrite(green_pin, int(green * 100))
+		pi.softPwmWrite(blue_pin, int(blue * 100))
+		pi.softPwmWrite(red_pin, int(red * 100))
+
+		time.sleep(0.01)
+except KeyboardInterrupt:
+	pass
+finally:
+	print("終了処理中・・・")
